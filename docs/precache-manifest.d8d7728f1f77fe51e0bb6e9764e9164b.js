6bc3a126f59e7d2ffb2c76f5b0417f5df70cd9c4
asima <asima@dcsrs-biosoda.ad.unil.ch>
2024-01-26 15:23:45 +0100

    update

---
+++ docs/precache-manifest.d8d7728f1f77fe51e0bb6e9764e9164b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0de108b9a7331ef1dea06ce38e68ea9",
+    "url": "/bioquery-dbgi/static/media/chevron-right.a0de108b.svg"
+  },
+  {
+    "revision": "b0ca7d8035e50297ae530cab9f85b139",
+    "url": "/bioquery-dbgi/static/media/chevron-down.b0ca7d80.svg"
+  },
+  {
+    "revision": "6c459f37792bb08ed82f",
+    "url": "/bioquery-dbgi/static/js/runtime~main.ae9de769.js"
+  },
+  {
+    "revision": "d3cc272b3a0e87fc4057",
+    "url": "/bioquery-dbgi/static/js/main.0e91f4a3.chunk.js"
+  },
+  {
+    "revision": "a8d9de32b4c8494e7e98",
+    "url": "/bioquery-dbgi/static/js/2.c4135753.chunk.js"
+  },
+  {
+    "revision": "d3cc272b3a0e87fc4057",
+    "url": "/bioquery-dbgi/static/css/main.6876212e.chunk.css"
+  },
+  {
+    "revision": "a8d9de32b4c8494e7e98",
+    "url": "/bioquery-dbgi/static/css/2.c1d20150.chunk.css"
+  },
+  {
+    "revision": "f4a7c7e1637a5acbe1d8b458521efa00",
+    "url": "/bioquery-dbgi/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dbgi_query/precache-manifest.0ec37d60c2fe3942cb3dcd398b330b93.js"
+  "/bioquery-dbgi/precache-manifest.d8d7728f1f77fe51e0bb6e9764e9164b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dbgi_query/index.html", {
+workbox.routing.registerNavigationRoute("/bioquery-dbgi/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
